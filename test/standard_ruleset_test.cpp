5c851d860c4fa203289b7a207e1fb8e6ae4182e9
Dmitry Isupov <TheApX@users.noreply.github.com>
2021-06-19 10:37:35 -0400

    Tests for placing snakes.

--- test/standard_ruleset_test.cpp
+++ test/standard_ruleset_test.cpp
@@ -17,6 +17,10 @@ using ::testing::ElementsAre;
 using ::testing::ElementsAreArray;
 using ::testing::Eq;
 using ::testing::Ge;
+using ::testing::Gt;
+using ::testing::IsFalse;
+using ::testing::IsTrue;
+using ::testing::Le;
 using ::testing::Lt;
 
 template <class T>
@@ -24,7 +28,17 @@ auto ValueBetween(const T& a, const T& b) {
   return AllOf(Ge(a), Lt(b));
 }
 
-class StandardRulesetTest : public testing::Test {};
+class StandardRulesetTest : public testing::Test {
+ protected:
+  std::vector<SnakeId> CreateSnakeIds(int n) {
+    std::vector<SnakeId> result;
+    result.reserve(n);
+    for (int i = 0; i < n; ++i) {
+      result.push_back("Snake" + std::to_string(n));
+    }
+    return result;
+  }
+};
 
 TEST_F(StandardRulesetTest, Sanity) {
   StandardRuleset ruleset;
@@ -39,7 +53,7 @@ TEST_F(StandardRulesetTest, Sanity) {
   EXPECT_THAT(state.height, Eq(0));
   EXPECT_THAT(state.snakes, ElementsAre());
 
-  EXPECT_THAT(ruleset.IsGameOver(state), Eq(true));
+  EXPECT_THAT(ruleset.IsGameOver(state), IsTrue());
 }
 
 class CreateInitialBoardStateTest : public StandardRulesetTest {
@@ -112,6 +126,124 @@ TEST_F(CreateInitialBoardStateTest, SmallBoard) {
               kBoardSizeSmall, kBoardSizeSmall, 3, {"one", "two"});
 }
 
+class PlaceSnakeTest : public StandardRulesetTest {
+ protected:
+  void ExpectBoardSnakes(const BoardState& state, int num_snakes) {
+    // Check that number of snakes is expected.
+    EXPECT_THAT(state.snakes.size(), Eq(num_snakes));
+
+    // Check each snake.
+    for (const Snake& snake : state.snakes) {
+      // Check that body size is expected.
+      EXPECT_THAT(snake.body.size(), Eq(3));
+
+      // Check that head is on an even cell.
+      ASSERT_THAT(snake.body.empty(), IsFalse());
+      Point prev = snake.body.front();
+      EXPECT_THAT((prev.x + prev.x) % 2, Eq(0));
+
+      for (const Point& p : snake.body) {
+        // Check that each body piece is within the board.
+        EXPECT_THAT(p.x, ValueBetween(0, state.width));
+        EXPECT_THAT(p.y, ValueBetween(0, state.height));
+        // Check that body pieces are connected or overlap.
+        EXPECT_THAT(std::abs(p.x - prev.x) + std::abs(p.y - prev.y), Le(0));
+      }
+    }
+  }
+};
+
+TEST_F(PlaceSnakeTest, Small1by1) {
+  StandardRuleset ruleset;
+  ExpectBoardSnakes(ruleset.CreateInitialBoardState(1, 1, CreateSnakeIds(1)),
+                    1);
+}
+
+TEST_F(PlaceSnakeTest, Small1by1TwoSnakes) {
+  StandardRuleset ruleset;
+  EXPECT_THROW(ruleset.CreateInitialBoardState(1, 1, CreateSnakeIds(2)),
+               ErrorNoRoomForSnake);
+}
+
+TEST_F(PlaceSnakeTest, Small1by2TwoSnakes) {
+  StandardRuleset ruleset;
+  EXPECT_THROW(ruleset.CreateInitialBoardState(1, 2, CreateSnakeIds(2)),
+               ErrorNoRoomForSnake);
+}
+
+TEST_F(PlaceSnakeTest, Small2by1TwoSnakes) {
+  StandardRuleset ruleset;
+  EXPECT_THROW(ruleset.CreateInitialBoardState(2, 1, CreateSnakeIds(2)),
+               ErrorNoRoomForSnake);
+}
+
+TEST_F(PlaceSnakeTest, Small2by2TwoSnakes) {
+  StandardRuleset ruleset;
+  ExpectBoardSnakes(ruleset.CreateInitialBoardState(2, 2, CreateSnakeIds(2)),
+                    2);
+}
+
+TEST_F(PlaceSnakeTest, EnoughSpaceForManySnakes) {
+  StandardRuleset ruleset;
+  ExpectBoardSnakes(ruleset.CreateInitialBoardState(5, 10, CreateSnakeIds(25)),
+                    25);
+}
+
+TEST_F(PlaceSnakeTest, NotEnoughSpaceForManySnakes) {
+  StandardRuleset ruleset;
+  EXPECT_THROW(ruleset.CreateInitialBoardState(5, 10, CreateSnakeIds(26)),
+               ErrorNoRoomForSnake);
+}
+
+TEST_F(PlaceSnakeTest, KnownSizeSmallOneSnake) {
+  StandardRuleset ruleset;
+  ExpectBoardSnakes(ruleset.CreateInitialBoardState(
+                        kBoardSizeSmall, kBoardSizeSmall, CreateSnakeIds(1)),
+                    1);
+}
+
+TEST_F(PlaceSnakeTest, KnownSizeSmallMaxSnakes) {
+  StandardRuleset ruleset;
+  ExpectBoardSnakes(ruleset.CreateInitialBoardState(
+                        kBoardSizeSmall, kBoardSizeSmall, CreateSnakeIds(8)),
+                    8);
+}
+
+TEST_F(PlaceSnakeTest, KnownSizeSmallTooManySnakes) {
+  StandardRuleset ruleset;
+  EXPECT_THROW(ruleset.CreateInitialBoardState(kBoardSizeSmall, kBoardSizeSmall,
+                                               CreateSnakeIds(9)),
+               ErrorTooManySnakes);
+}
+
+TEST_F(PlaceSnakeTest, KnownSizeMediumMaxSnakes) {
+  StandardRuleset ruleset;
+  ExpectBoardSnakes(ruleset.CreateInitialBoardState(
+                        kBoardSizeMedium, kBoardSizeMedium, CreateSnakeIds(8)),
+                    8);
+}
+
+TEST_F(PlaceSnakeTest, KnownSizeMediumTooManySnakes) {
+  StandardRuleset ruleset;
+  EXPECT_THROW(ruleset.CreateInitialBoardState(
+                   kBoardSizeMedium, kBoardSizeMedium, CreateSnakeIds(9)),
+               ErrorTooManySnakes);
+}
+
+TEST_F(PlaceSnakeTest, KnownSizeLargeMaxSnakes) {
+  StandardRuleset ruleset;
+  ExpectBoardSnakes(ruleset.CreateInitialBoardState(
+                        kBoardSizeLarge, kBoardSizeLarge, CreateSnakeIds(8)),
+                    8);
+}
+
+TEST_F(PlaceSnakeTest, KnownSizeLargeTooManySnakes) {
+  StandardRuleset ruleset;
+  EXPECT_THROW(ruleset.CreateInitialBoardState(kBoardSizeLarge, kBoardSizeLarge,
+                                               CreateSnakeIds(9)),
+               ErrorTooManySnakes);
+}
+
 class PlaceFoodTest : public StandardRulesetTest {
  protected:
   void ExpectBoardFood(const BoardState& state, int num_food) {
@@ -120,15 +252,6 @@ class PlaceFoodTest : public StandardRulesetTest {
       EXPECT_THAT(food.x, ValueBetween(0, state.width));
       EXPECT_THAT(food.y, ValueBetween(0, state.height));
     }
-  }
-
-  std::vector<SnakeId> CreateSnakeIds(int n) {
-    std::vector<SnakeId> result;
-    result.reserve(n);
-    for (int i = 0; i < n; ++i) {
-      result.push_back("Snake" + std::to_string(n));
-    }
-    return result;
   }
 };
 
